3b465d0e37641a66a21fa03ebce1b46d59f95285
EduardoSantosOliveira <102029667+EduardoSantosOliveira@users.noreply.github.com>
2022-03-29 08:26:08 -0300

    Add files via upload

---
+++ forca.h
@@ -0,0 +1,12 @@
+
+void adicionapalavra();
+void abertura ();
+int enforcou();
+void escolhepalavra();
+void chuta();
+void desenhaforca();
+int jachutou(char letra);
+int ganhou();
+int letraexiste(char letra);
+int chuteerrados();
+void desenhaforca();
